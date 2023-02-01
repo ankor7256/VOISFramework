de9f55de5bb9a23776a53b8e7ae9eb4e8de044a0
Коротков Андрей <andrey.korotkov@joy-dev.com>
2023-02-01 15:05:08 -0500

    first commit

--- Sources/VOISFramework/VOISFramework.swift
+++
@@ -1,6 +0,0 @@
-public struct VOISFramework {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- Tests/VOISFrameworkTests/VOISFrameworkTests.swift
+++ Tests/VOISFrameworkTests/VOISFrameworkTests.swift
@@ -6,6 +6,5 @@ final class VOISFrameworkTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(VOISFramework().text, "Hello, World!")
     }
 }
